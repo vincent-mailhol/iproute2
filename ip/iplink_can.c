e46ad74722f48ce3cadb96d236d89ceefdecb8a8
Vincent Mailhol <mailhol@kernel.org>
2025-08-21 18:43:34 +0900

    iplink_can: add CAN_CTRLMODE_XL_ERR_SIGNAL
    Signed-off-by: Vincent Mailhol <mailhol@kernel.org>

--- ip/iplink_can.c
+++ ip/iplink_can.c
@@ -50,6 +50,7 @@ static void print_usage(FILE *f)
 		"\t[ xl { on | off } ]\n"
 		"\t[ xtdc-mode { auto | manual | off } ]\n"
 		"\t[ tms { on | off } ]\n"
+		"\t[ err-signal { on | off } ]\n"
 		"\n"
 		"\t[ restart-ms TIME-MS ]\n"
 		"\t[ restart ]\n"
@@ -135,6 +136,7 @@ static void print_ctrlmode(enum output_type t, __u32 flags, const char* key)
 	print_flag(t, &flags, CAN_CTRLMODE_XL_TDC_AUTO, "XL-TDC-AUTO");
 	print_flag(t, &flags, CAN_CTRLMODE_XL_TDC_MANUAL, "XL-TDC-MANUAL");
 	print_flag(t, &flags, CAN_CTRLMODE_XL_TMS, "XL-TMS");
+	print_flag(t, &flags, CAN_CTRLMODE_XL_ERR_SIGNAL, "XL-ERR-SIGNAL");
 
 	if (flags)
 		print_hex(t, NULL, "%x", flags);
@@ -344,6 +346,9 @@ static int can_parse_opt(struct link_util *lu, int argc, char **argv,
 		} else if (matches(*argv, "tms") == 0) {
 			NEXT_ARG();
 			set_ctrlmode("tms", *argv, &cm, CAN_CTRLMODE_XL_TMS);
+		} else if (matches(*argv, "err-signal") == 0) {
+			NEXT_ARG();
+			set_ctrlmode("err-signal", *argv, &cm, CAN_CTRLMODE_XL_ERR_SIGNAL);
 		} else if (matches(*argv, "restart") == 0) {
 			__u32 val = 1;
 
